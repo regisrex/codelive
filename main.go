3e4988448c73c5d4cb691fc32f90fa8407ec791f
ndzhwr <ndizihiweregis06@gmail.com>
2023-04-19 09:00:17 +0200

    refact: css - html code seperation

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"net/http"
 	"sharecode/controllers"
 	"sharecode/utils"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// loading environment varibles
 	utils.LoadEnv()
 	utils.PayloadUpgrader()
+
 	// initialize a gin router.
 	router := gin.Default()
 
+	// static files setup
+	// fs := http.FileServer(http.Dir("assets"))
+	// http.Handle("/st/", http.StripPrefix("/st/", fs))
+
+	router.GET("/css", func(c *gin.Context) {
+		http.ServeFile(c.Writer ,c.Request, "assets/style.css" )
+	})
 	// routes
 	router.GET("/", controllers.MainRoute)
 	router.GET("/s/:id", controllers.NewSnippet)
